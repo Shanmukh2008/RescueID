2ddaeea4601f4957aeb63dd391241215331b3a6b
Shanmukh Praharaju <tarunshanmukh2008@gmail.com>
2026-06-03 12:29:17 +0530

    update CORS for local network access

--- server.js
+++ server.js
@@ -10,7 +10,10 @@ require('./models/EmergencyContact');
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:3000', 'http://10.1.11.43:3000'],
+  credentials: true
+}));
 app.use(express.json());
 
 // Routes
