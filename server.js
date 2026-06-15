74bc0661c910ba1c08f0fec3d0f38bb669ddedaf
Shanmukh Praharaju <tarunshanmukh2008@gmail.com>
2026-06-15 10:38:25 +0530

    fix CORS with manual headers

--- server.js
+++ server.js
@@ -8,6 +8,15 @@ require('./models/User');
 require('./models/EmergencyContact');
 
 const app = express();
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  if (req.method === 'OPTIONS') {
+    return res.sendStatus(200);
+  }
+  next();
+});
 
 // Middleware
 app.use(cors({
