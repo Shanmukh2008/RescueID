515572fcf680eee2de69984f1325fdbc9607e08a
Shanmukh Praharaju <tarunshanmukh2008@gmail.com>
2026-06-15 10:35:33 +0530

    fix CORS

--- server.js
+++ server.js
@@ -11,16 +11,9 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: [
-    'http://localhost:3000',
-    'https://rescueid.netlify.app',
-    'http://rescueid.tech',
-    'https://rescueid.tech',
-    'http://www.rescueid.tech',
-    'https://www.rescueid.tech'
-  ],
-  credentials: true
-}));;
+  origin: '*',
+  credentials: false
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
