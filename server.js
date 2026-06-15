9bac755aac854653ed65b0ee2cfbb32e5c18d332
Shanmukh Praharaju <tarunshanmukh2008@gmail.com>
2026-06-15 14:21:53 +0530

    force sequelize sync to update model

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ const PORT = process.env.PORT || 8000;
 sequelize.authenticate()
   .then(() => {
     console.log('PostgreSQL connected');
-    return sequelize.sync();
+    return sequelize.sync({ alter: true });
   })
   .then(() => {
     app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
